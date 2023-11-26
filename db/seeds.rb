4956ff711a4a6c770757ab4d8ee985aba2a12b1b
go2023 <tetyanago2018@gmail.com>
2023-11-25 16:42:32 -0800

    Gem devise was added, models were created, db was seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.first_or_create(email: 'tetyana@example.com',
+                     password: 'password',
+                     password_confirmation: 'password',
+                     role: User.roles[:admin])
